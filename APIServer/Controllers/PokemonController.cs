587ae67b6dcd3ecaa395e282794fd5e89783d01a
Pontus <02pontuskarlsson@gmail.com>
2021-03-16 11:08:38 +0100

    Innitial commit

---
+++ APIServer/Controllers/PokemonController.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace APIServer.Controllers
+{
+
+    [ApiController]
+    [Route("api/pokemon")]
+    public class PokemonController: ControllerBase
+    {
+
+        static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "Pikachu"},
+            new Pokemon() {Name = "Charmander"}
+        };
+
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(pokemons[1]);
+        }
+        
+    }
+}
